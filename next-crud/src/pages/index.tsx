5c25946ee02ebf08a49c6d9e26a1231f00254a6a
jpvieira1 <jp201101@gmail.com>
2022-03-29 13:03:59 -0300

    Componente Botão

--- next-crud/src/pages/index.tsx
+++ next-crud/src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Botao from "../components/Botao"
 import Cliente from "../components/core/Cliente"
 import Layout from "../components/Layout"
 import Tabela from "../components/Tabela"
@@ -26,6 +27,9 @@ export default function Home() {
       text-white
     `}>
       <Layout title="Cadastro simples">
+        <div className="flex justify-end" >
+        <Botao cor="green" className="mb-4" >Novo Cliente</Botao>
+        </div>
       <Tabela clientes={clientes} clienteSelecionado={clienteSelecionado} clienteExcluido={clienteExcluido} ></Tabela>
       </Layout>
     </div>
--- next-crud/tailwind.config.js
+++ next-crud/tailwind.config.js
@@ -1,8 +1,15 @@
 module.exports = {
+  purge: {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
+  safelist: [
+    /^bg-/,
+    /^to-/,
+    /^from-/,
+  ]
+},
   theme: {
     extend: {},
   },
